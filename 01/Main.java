20b5c99902c0e1b0ca389ed5fc69a7a415e885c2
adityathombre04 <adityathombre1440@gmail.com>
2024-01-13 09:49:13 +0530

    code added

---
+++ 01/Main.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main{
+  public static void main(String args[]){
+    Scanner in = new Scanner(System.in);
+    Random random = new Random();
+    //initial values
+    int user = 0;
+    int computer = 1;
+    int score = 0;
+    int limit = 5;
+    int lives = 3;
+      //limiting the number of guess user has
+      for(int i = lives; i > 0; i--){
+        if(user != computer){
+          System.out.println("Lives: " + i );
+            System.out.println("Enter a Random Number from 1 to "+limit+": ");
+            user = in.nextInt();
+            //to clear the new-line attribute from the input buffer
+            in.nextLine();
+          
+            computer = random.nextInt(limit);
+            System.out.println("User: "+user+" Computer: "+computer);
+
+            if(user == computer){
+              System.out.println("Guess Correct✅");
+              score +=10;
+              lives += 1;
+            }
+            //game-over if user chances are over
+            else if(i == 1){
+              System.out.println("You Lost this round😭");
+              break;
+            }
+            else if(user > computer){
+              System.out.println("Too High, Try Again... \n");
+              score -=2;
+              lives -= 1;
+            }
+            else if(user < computer){
+              System.out.println("Too Low, Try Again... \n");
+              score -=2;
+              lives -= 1;
+            }
+          }
+      }
+in.close();
+  }
+}
